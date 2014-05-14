3685cec83c10dc6f348fe902442811ea23359dd3
Prateek <prateek@citrusleaf.com>
2014-05-14 12:07:37 +0530

    Improved readme and added a run_all script for the full automation of demo.

---
+++ run_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Requirement - namespace named as "test"
+
+aql -c "create index ind on test.demo (write_time) numeric"
+java -jar aerospike-load-1.0-jar-with-dependencies.jar  -wt 1 -rt 1 -l 1 -c moviereview.json moviedata.csv -s demo 2>&1 > /dev/null &
+
+echo " " > /tmp/aql_commands
+cmd="set lua_userpath 'lua_files'"
+echo $cmd >> /tmp/aql_commands
+cmd="register module 'lua_files/movies.lua'"
+echo $cmd >> /tmp/aql_commands
+
+watch -d 'python most_reviewed.py 10'
+
+
+
+
